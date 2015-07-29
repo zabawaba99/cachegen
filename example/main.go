d374546186c979362350383524e60e9fb5dfc62e
Steven Berlanga <zabawaba99@gmail.com>
2015-07-29 13:15:22 -0400

    adding a short main function in example

--- example/main.go
+++ example/main.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"log"
+	"time"
+)
+
 //go:generate cachegen -key-type=string -value-type=int
 
 //go:generate cachegen -key-type=int -value-type=Customer
 type Customer struct {
 	ID int
+}
+
+func main() {
+	cc := NewCustomerCache(time.Second, 10*time.Second)
+
+	cust := Customer{ID: 42}
+	cc.Add(cust.ID, cust)
+
+	time.Sleep(time.Second)
+
+	_, ok := cc.Get(cust.ID)
+	log.Println("Customer is expired:", !ok)
 }
